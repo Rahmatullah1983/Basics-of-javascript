35a5e247771be6f11a1f723b2a9e01f689a22141
Rahmatullah1983 <bugtirahmatullah@gmail.com>
2023-07-18 09:47:53 +0500

    one js file added to about.html

---
+++ day1.js
@@ -0,0 +1,17 @@
+//let age =15;
+//if( age >=18){
+ //   alert("you are welcome");
+//}
+//else{
+   // alert( "you are so young you can not enter");
+//}
+//let a =10;
+
+//alert( a+5);
+//alert( a/3);
+//alert (a*3);
+//alert ( a%3);
+let ubl= 5000;
+let nbp =5000;
+let allBanks=(ubl+nbp);
+alert(allBanks);

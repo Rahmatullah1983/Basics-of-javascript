b51ec90adbccbef5dbedbed5a2ac64d2a4df6eb9
Rahmatullah1983 <bugtirahmatullah@gmail.com>
2023-07-20 13:12:54 +0500

    today I have learnet about variables and const

--- day1.js
+++ day1.js
@@ -1,26 +1,15 @@
-//let age =15;
-//if( age >=18){
- //   alert("you are welcome");
-//}
-//else{
-   // alert( "you are so young you can not enter");
-//}
-//let a =10;
-
-//alert( a+5);
-//alert( a/3);
-//alert (a*3);
-//alert ( a%3);
-//let assets= 10000;
-//let liabilities =3000;
-//let netWorth=(assets-liabilities);
-//alert(netWorth);
-
-let age =18;
-let bank =15;
-if(age> 18 && bank > 15){
-   alert(you are welcome);
-}else{
-   alert(you connot enter);
-   
-}
+let inbox=100;
+alert (inbox);
+alert (inbox+1);
+let inbox1 = 200;
+let allEmails = inbox + inbox1;
+alert (allEmails);
+allEmails = 500;
+alert (allEmails);
+let inbox;
+inbox =100;
+//the value of veriabls can be change.
+const class1 = 100;
+const class2 = 200;
+const allStudents = inbox + inbox1;
+alert (allStudents);
